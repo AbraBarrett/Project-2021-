b6606bc4004709d32fc63b1490641cc980f96c14
AbraBarrett <94629103+AbraBarrett@users.noreply.github.com>
2021-11-18 21:34:20 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/mainpage')
+def main_page():
+    return render_template('mainpage.html')
+
+@app.route('/vulnerabilities')
+def contacts():
+    return render_template('vulnerabilities.html')
+
+@app.route('/antiviruses')
+def new_things():
+    return render_template('antiviruses.html')
+
+if __name__ == '__main__':
+    app.run()
