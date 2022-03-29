69af1a7e8d2b559fdcc51d3504a4125606f9e99a
AbraBarrett <94629103+AbraBarrett@users.noreply.github.com>
2022-03-29 15:38:10 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/mainpage',methods=['POST'])
+@app.route('/mainpage')
 def main_page():
     try:
         if request.method=='post':
